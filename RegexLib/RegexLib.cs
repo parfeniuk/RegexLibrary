25e653e85cacf088300ef15fcdcceab114fc3f12
Oleksandr P <o.parfeniuk@gmail.com>
2017-09-25 11:07:12 +0300

    Sync dev with master

--- RegexLib/RegexLib.cs
+++ RegexLib/RegexLib.cs
@@ -7,23 +7,18 @@ using System.Text.RegularExpressions;
 
 namespace RegexLib
 {
-	public static class RegexLib
+    public static class RegexLib
     {
-<<<<<<< HEAD
-
+        public static bool IsAddress(this string str)
+        {
+            if (Regex.IsMatch(str, @"/@/", RegexOptions.IgnoreCase))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
-=======
-		public static bool IsAddress(this string str)
-		{
-			if (Regex.IsMatch(str, @"/@/", RegexOptions.IgnoreCase))
-			{
-				return true;
-			}
-			else
-			{
-				return false;
-			}
-		}
-	}
->>>>>>> b17e5567ba4e4b7b8c7fc3c5756cfae5b459e206
 }
